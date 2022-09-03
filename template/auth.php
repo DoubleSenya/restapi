cdc7c77ac1fb24edeecc098a99b560c337e3eadb
DoubleSenya <korotitsky.konstantin@gmail.com>
2022-09-04 00:10:12 +0700

    html

---
+++ template/auth.php
@@ -0,0 +1,11 @@
+<form>
+  <div class="mb-3">
+    <label for="login" class="form-label">Логин</label>
+    <input type="text" class="form-control" id="login">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Пароль</label>
+    <input type="password" class="form-control" id="password">
+  </div>
+  <button type="submit" class="btn btn-primary">Войти</button>
+</form>
---
+++ template/main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>REST API</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+            rel="stylesheet" 
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+            crossorigin="anonymous">
+        <style>
+            body{
+                height: 100vh;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            form{
+                width: 500px;
+            }
+
+            p{
+                font-size: 50px;
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                opacity: 0.2;
+                user-select: none;
+                color: blue;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            require "auth.php";
+        ?>
+    </body>
+</html>
